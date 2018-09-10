75d040b715ab61c7e529a5c33360498e88a5bc65
Andri Kaljumäe <andrikaljumae@users.noreply.github.com>
2018-09-10 10:40:54 +0300

    html muudetud php-ks

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Andri Kaljumäe, õppetöö</title>
+</head>
+<body>
+  <h1>Andri Kaljumäe</h1>
+  <p>Siin on minu <a href="http://www.tlu.ee" target="_blank">TLÜ</a> õppetöö raames valminud veebilehed. Need ei oma mingit sügavat sisu ja nende kopeerimine ei oma mõtet.</p>
+  <p>Kodutöö.</p>
+<!--<img src=
+"http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_3.jpg" alt="TLÜ Terra Õppehoone">-->
+<img src="../../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_3.jpg" alt="TLÜ Terra Õppehoone">
+<p> Mul on sõber, kes ka teeb <a href="../../~merivor" target="_blank">veebi</a>.</p>
+
+</body>
+</html>
